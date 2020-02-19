dc187d3c0bae793149340a0f21ae08ddc8c84f62
Avnika Manaktala <amanakta@brown-fe01.rcac.purdue.edu>
2020-02-19 16:47:46 -0500

    l Final Program and Files for Lab 5

--- bash_tutorial/ProcessStations.sh
+++ bash_tutorial/ProcessStations.sh
@@ -25,25 +25,35 @@ done
 
 ## Extracting Longitude and Latitude Data
 
-awk '/Longitude/ {print -1 * $NF}' StationData/Station_*.txt > Long.list
-awk '/Latitude/ {print -1 * $NF}' StationData/Station_*.txt > Lat.list
-awk '/Longitude/ {print -1 * $NF}' HigherElevation/Station_*.txt > LongHE.list
-awk '/Latitude/ {print -1 * $NF}' HigherElevation/Station_*.txt > LatHE.list
-paste Long.list Lat.list > AllStations.xy
-paste LongHE.list LatHE.list > HEStations.xy
+awk '/Longitude/ {print -1 * $NF}' StationData/Station_*.txt > Long.list 
+awk '/Latitude/ {print $NF}' StationData/Station_*.txt > Lat.list 
+paste Long.list Lat.list > AllStations.xy 
+awk '/Longitude/ {print -1 * $NF}' HigherElevation/Station_*.txt > HELong.list
+awk '/Latitude/ {print $NF}' HigherElevation/Station_*.txt > HELat.list
+paste HELong.list HELat.list > HEStations.xy
 
 ## Loading gmt package
 
-. /etc/profile
 module load gmt
 
 ## Plotting Figure
 
-gmt pscoast -JU16/4i -R-93/-86/36/43 -B2f0.5 -Cl/bue -Dh[+] -Ia/blue -Na/orange -P -K -V > SoilMoistureStations.ps
+gmt pscoast -JU16/4i -R-93/-86/36/43 -B2f0.5 -Cl/blue -Dh[+] -Ia/blue -Na/orange -P -K -V > SoilMoistureStations.ps
 gmt psxy AllStations.xy -J -R -Sc0.15 -Gblack -K -O -V >> SoilMoistureStations.ps
-gmt psxy HEStations.xy -J -R -Sc0.15 -Gred -O -V >> SoilMoistureStations.ps
+gmt psxy HEStations.xy -J -R -Sc0.07 -Gred -O -V >> SoilMoistureStations.ps
 
 ## Displaying Figure
 
 gv SoilMoistureStations.ps &
+
+
+# Part III: Convert the figure into other image formats
+
+## Converting to epsi format
+
+ps2epsi SoilMoistureStations.ps
+
+## Converting to tiff format
+
+convert -density 150x150 SoilMoistureStations.epsi SoilMoistureStations.tiff
 
